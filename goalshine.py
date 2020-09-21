8a122ed94c15250051523f2407e81e90dbe0cfe1
HotDogGuy <46381224+HotDogGuy@users.noreply.github.com>
2020-09-21 11:10:52 -0500

    changed exit to sys.exit

--- goalshine.py
+++ goalshine.py
@@ -6,14 +6,14 @@ def goalshineinput(): #gets gfx names for print
     global gfxName #name of file without .dds
     gfxName = input("Enter GFX name exluding .dds or other file format. Type quit to quit.")
     if gfxName in {'quit', 'Quit', 'QUIT'}: #used to exit if quit is typed in
-        exit('Goodbye')
+        sys.exit('Goodbye')
     else:
         global gfxNameFile #name of file with .dds
         gfxNameFile = input("Enter GFX name including .dds or other file format. Type quit to quit.")
         confirmation()
 def confirmation(): #confirms the user wants those, if not, restarts goalshineinput
     if gfxNameFile in {'Quit', 'quit', 'QUIT'}: #used to exit if quit is typed in
-        exit('Goodbye')
+        sys.exit('Goodbye')
     else:
         print(gfxName) #for testing, combine into complete sentence
         print(gfxNameFile) #for testing, combine into complete sentence
