76c1c79aadc4f97e8185704b1856846ff3986461
HotDogGuy <46381224+HotDogGuy@users.noreply.github.com>
2020-10-01 08:28:42 -0500

    added docstrings to functions and global variables

--- goalshine.py
+++ goalshine.py
@@ -1,15 +1,19 @@
 import time
 import sys
 def goalshineinput(): #gets gfx names for print
+    """This function takes the gfx names and saves them for later"""
     global gfxName #name of file without .dds
+    """Name of file without .dds"""
     gfxName = input("Enter GFX name exluding .dds or other file format. Type quit to quit.")
     if gfxName in {'quit', 'Quit', 'QUIT'}: #used to exit if quit is typed in
         sys.exit('Goodbye')
     else:
         global gfxNameFile #name of file with .dds
+        """Name of file with .dds"""
         gfxNameFile = input("Enter GFX name including .dds or other file format. Type quit to quit.")
         confirmation()
 def confirmation(): #confirms the user wants those, if not, restarts goalshineinput
+    """This function confirms the users inputs and restarts goalshineinput if they don't want them"""
     if gfxNameFile in {'Quit', 'quit', 'QUIT'}: #used to exit if quit is typed in
         sys.exit('Goodbye')
     else:
@@ -26,6 +30,7 @@ def confirmation(): #confirms the user wants those, if not, restarts goalshinein
             time.sleep(2)
             confirmation()
 def goalshinework(): #creates the thing to be printed
+    """Prints the text needed for goalshine"""
     print('SpriteType = {')
     print('\t''name = ''\"' + gfxName + '\"')
     print('\t''texturefile = ''\"''gfx/interface/goals/' + gfxNameFile + '\"')
